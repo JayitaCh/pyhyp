69100b07a9a180dbf41a026035d1b30a4ce7b2c1
Jayita Chakraborty <51598788+JayitaCh@users.noreply.github.com>
2024-10-14 10:36:39 +0000

    Created new Python file for estimating the longest side of a rt-angled triangle.
    Following the instructions of good software-dev
    practices from the following url:
    https://software-dev.streamlit.app/Project_Workflow

---
+++ src/pyhyp/pythag.py
@@ -0,0 +1,43 @@
+# Module to calculate the longest side of a right-angled 
+# triangle using Pythogora's theorem.
+
+#Function to check input is float/int
+#throw error if not
+
+# Function to add two numbers together
+# inputs: float or int or list/ array of float or int
+
+# Generate Docstring added in devcontainer.json to automatically get func description
+def add_nums(a,b):
+    """_summary_
+
+    Args:
+        a (float, int, array): a number/list of numbers
+        b (float, int, array): a number/list of numbers
+
+    Returns:
+        float/int/array: Addition of a and b
+    """
+    sum = a+b
+    return sum
+
+# Function to calculate the square of the number
+# inputs: float or int or list/ array of float or int
+def square_num(a):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    sq_num = a**2
+    return sq_num
+
+# Function to calculate the square root of the number
+
+# Function to calculate the hypotenuse using the following equation
+# a^2 + b^2= c^2
+# c = sqrt(a^2 + b^2)
+# inputs: float or int or list/ array of float or int
